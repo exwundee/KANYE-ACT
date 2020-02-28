8436d1e3df659ca0caa54403321c4650e43beafe
Nicholas <36908791+exwundee@users.noreply.github.com>
2020-02-28 10:47:39 -0500

    Add files via upload

---
+++ KANYE ACT/KANYE ACT/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Class MainWindow
+    Private Sub LstKayaks_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles lstKayaks.SelectionChanged
+        imgRecKayak.Visibility = Visibility.Hidden
+        imgRecTandem.Visibility = Visibility.Hidden
+        imgSeaKayak.Visibility = Visibility.Hidden
+
+        If lstKayaks.SelectedIndex = 0 Then
+            imgRecKayak.Visibility = Visibility.Visible
+        ElseIf lstKayaks.SelectedIndex = 1 Then
+            imgRecTandem.Visibility = Visibility.Visible
+        ElseIf lstKayaks.SelectedIndex = 2 Then
+            imgSeaKayak.Visibility = Visibility.Visible
+        End If
+    End Sub
+End Class
